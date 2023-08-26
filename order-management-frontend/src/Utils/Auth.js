a017b75b537e1f3059604548666af2f05c561dee
Givitha2005 <115726408+Givitha2005@users.noreply.github.com>
2023-08-26 20:18:12 +0530

    Order Management

---
+++ order-management-frontend/src/Utils/Auth.js
@@ -0,0 +1,9 @@
+export const isAuthenticated = () => {
+    if (typeof window === "undefined") {
+      return false;
+    }
+    if (localStorage.getItem("token")) {
+      return JSON.parse(localStorage.getItem("token"));
+    }
+  };
+  
